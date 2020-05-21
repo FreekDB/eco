8e2648266614352b024e0a3bb4af27c5e03bc0d5
Freek de Bruijn <freekdb@gmail.com>
2020-05-21 22:27:54 +0200

    Add Gradle build files.

---
+++ build.gradle.kts
@@ -0,0 +1,29 @@
+group = "com.github.freekdb.eco"
+version = "0.0.6-SNAPSHOT"
+
+plugins {
+    java
+    kotlin("jvm") version "1.4-M1"
+}
+
+repositories {
+    maven("https://dl.bintray.com/kotlin/kotlin-eap")
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+}
+
+configure<JavaPluginConvention> {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "11"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "11"
+    }
+}
